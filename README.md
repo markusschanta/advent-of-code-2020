6849885e275a1d500c91341f146843524aae3131
Markus Schanta <markus@schanta.at>
2020-12-15 10:15:45 +0000

    Add main file

---
+++ README.md
@@ -0,0 +1,16 @@
+---
+jupyter:
+  jupytext:
+    formats: ipynb,md,py:percent
+    text_representation:
+      extension: .md
+      format_name: markdown
+      format_version: '1.2'
+      jupytext_version: 1.4.2
+  kernelspec:
+    display_name: Python 3
+    language: python
+    name: python3
+---
+
+# Advent of Code 2020
---
+++ README.py
@@ -0,0 +1,17 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,md,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.4.2
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # Advent of Code 2020
