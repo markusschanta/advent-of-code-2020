dc2f8c58720643deec860deb7161ed411696ab05
Markus Schanta <markus@schanta.at>
2020-12-15 16:27:40 +0000

    Add day 3

--- README.py
+++ README.py
@@ -149,6 +149,140 @@ def valid2(input_line):
 np.sum([valid2(i) for i in input])
 
 # %% [markdown]
+# ## Day 3: Toboggan Trajectory
+#
+# With the toboggan login problems resolved, you set off toward the airport. While travel by toboggan might be easy, it's certainly not safe: there's very minimal steering and the area is covered in trees. You'll need to see which angles will take you near the fewest trees.
+#
+# Due to the local geology, trees in this area only grow on exact integer coordinates in a grid. You make a map (your puzzle input) of the open squares (`.`) and trees (`#`) you can see. For example:
+#
+#     ..##.......
+#     #...#...#..
+#     .#....#..#.
+#     ..#.#...#.#
+#     .#...##..#.
+#     ..#.##.....
+#     .#.#.#....#
+#     .#........#
+#     #.##...#...
+#     #...##....#
+#     .#..#...#.#
+#
+# These aren't the only trees, though; due to something you read about once involving arboreal genetics and biome stability, the same pattern repeats to the right many times:
+#
+#     ..##.........##.........##.........##.........##.........##.......  --->
+#     #...#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
+#     .#....#..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
+#     ..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
+#     .#...##..#..#...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
+#     ..#.##.......#.##.......#.##.......#.##.......#.##.......#.##.....  --->
+#     .#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
+#     .#........#.#........#.#........#.#........#.#........#.#........#
+#     #.##...#...#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...
+#     #...##....##...##....##...##....##...##....##...##....##...##....#
+#     .#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#  --->
+#
+# You start on the open square (`.`) in the top-left corner and need to reach the bottom (below the bottom-most row on your map).
+#
+# The toboggan can only follow a few specific slopes (you opted for a cheaper model that prefers rational numbers); start by **counting all the trees** you would encounter for the slope **right 3, down 1**:
+#
+# From your starting position at the top-left, check the position that is right `3` and down `1`. Then, check the position that is right `3` and down `1` from there, and so on until you go past the bottom of the map.
+#
+# The locations you'd check in the above example are marked here with `O` where there was an open square and `X` where there was a tree:
+#
+#     ..##.........##.........##.........##.........##.........##.......  --->
+#     #..O#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
+#     .#....X..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
+#     ..#.#...#O#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
+#     .#...##..#..X...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
+#     ..#.##.......#.X#.......#.##.......#.##.......#.##.......#.##.....  --->
+#     .#.#.#....#.#.#.#.O..#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
+#     .#........#.#........X.#........#.#........#.#........#.#........#
+#     #.##...#...#.##...#...#.X#...#...#.##...#...#.##...#...#.##...#...
+#     #...##....##...##....##...#X....##...##....##...##....##...##....#
+#     .#..#...#.#.#..#...#.#.#..#...X.#.#..#...#.#.#..#...#.#.#..#...#.#  --->
+#
+# In this example, traversing the map using this slope would cause you to encounter `7` trees.
+#
+# Starting at the top-left corner of your map and following a slope of right `3` and down `1`, **how many trees would you encounter?**
+
+# %%
+input = [
+    "..##.......",
+    "#...#...#..",
+    ".#....#..#.",
+    "..#.#...#.#",
+    ".#...##..#.",
+    "..#.##.....",
+    ".#.#.#....#",
+    ".#........#",
+    "#.##...#...",
+    "#...##....#",
+    ".#..#...#.#",
+]
+
+# %%
+input = open('input.3.txt', 'r').readlines()
+input = [i.strip() for i in input]
+
+
+# %%
+def is_tree(x, y):
+    line = input[y]
+    return line[x % len(line)] == '#'
+
+trees = 0
+
+for i in range(len(input)):
+    if is_tree(3 * i, i):
+        trees += 1
+
+trees
+
+
+# %% [markdown]
+# ### Part Two
+#
+# Time to check the rest of the slopes - you need to minimize the probability of a sudden arboreal stop, after all.
+#
+# Determine the number of trees you would encounter if, for each of the following slopes, you start at the top-left corner and traverse the map all the way to the bottom:
+#
+# * Right 1, down 1.
+# * Right 3, down 1. (This is the slope you already checked.)
+# * Right 5, down 1.
+# * Right 7, down 1.
+# * Right 1, down 2.
+#
+# In the above example, these slopes would find `2`, `7`, `3`, `4`, and `2` tree(s) respectively; multiplied together, these produce the answer **`336`**.
+#
+# **What do you get if you multiply together the number of trees encountered on each of the listed slopes?**
+#
+# Your puzzle answer was `2122848000`.
+
+# %%
+def num_trees(v_x, v_y):
+    x = 0
+    y = 0
+    trees = 0
+    while y < len(input):
+        trees += is_tree(x, y)
+        x += v_x
+        y += v_y
+    return trees
+
+num_trees(3, 1)
+
+# %%
+speeds = [
+    (1, 1),
+    (3, 1),
+    (5, 1),
+    (7, 1),
+    (1, 2)
+]
+
+np.prod([num_trees(*s) for s in speeds])
+
+# %% [markdown]
 # ## Day 15: Rambunctious Recitation
 #
 # You catch the airport shuttle and try to book a new flight to your vacation island. Due to the storm, all direct flights have been cancelled, but a route is available to get around the storm. You take it.
