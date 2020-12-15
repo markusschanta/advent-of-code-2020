57f42fb963fdd64abeea28b965971d268b59d568
Markus Schanta <markus@schanta.at>
2020-12-15 12:54:46 +0000

    Day 1: add answers

--- README.md
+++ README.md
@@ -47,6 +47,8 @@ In this list, the two entries that sum to `2020` are `1721` and `299`. Multiplyi
 
 Of course, your expense report is much larger. **Find the two entries that sum to `2020`; what do you get if you multiply them together?**
 
+Your puzzle answer was `224436`.
+
 ```python
 input = [1721, 979, 366, 299, 675, 1456]
 ```
@@ -70,6 +72,8 @@ The Elves in accounting are thankful for your help; one of them even offers you 
 Using the above example again, the three entries that sum to `2020` are `979`, `366`, and `675`. Multiplying them together produces the answer, **`241861950`.**
 
 In your expense report, **what is the product of the three entries that sum to `2020`?**
+
+Your puzzle answer was `303394260`.
 
 ```python
 for i in input:
--- README.py
+++ README.py
@@ -43,6 +43,8 @@
 # In this list, the two entries that sum to `2020` are `1721` and `299`. Multiplying them together produces `1721 * 299 = 514579`, so the correct answer is `514579`.
 #
 # Of course, your expense report is much larger. **Find the two entries that sum to `2020`; what do you get if you multiply them together?**
+#
+# Your puzzle answer was `224436`.
 
 # %%
 input = [1721, 979, 366, 299, 675, 1456]
@@ -65,6 +67,8 @@ for i in input:
 # Using the above example again, the three entries that sum to `2020` are `979`, `366`, and `675`. Multiplying them together produces the answer, **`241861950`.**
 #
 # In your expense report, **what is the product of the three entries that sum to `2020`?**
+#
+# Your puzzle answer was `303394260`.
 
 # %%
 for i in input:
