16c6216abaf2ca862283f320b0fa24fa2482b4ca
Markus Schanta <markus@schanta.at>
2020-12-17 08:41:26 +0000

    Add day 16

--- README.py
+++ README.py
@@ -23,6 +23,7 @@
 # * [Day 3: Toboggan Trajectory](#day-3-toboggan-trajectory)
 # * [Day 4: Passport Processing](#day-4-passport-processing)
 # * [Day 15: Rambunctious Recitation](#day-15-rambunctious-recitation)
+# * [Day 16: Ticket Translation](#day-16-ticket-translation)
 
 # %% [markdown]
 # ## Day 1: Report Repair
@@ -637,3 +638,154 @@ for i in range(1, n + 1):
             mentions[age] = [i]
 
 last
+
+# %% [markdown]
+# ## Day 16: Ticket Translation
+#
+# As you're walking to yet another connecting flight, you realize that one of the legs of your re-routed trip coming up is on a high-speed train. However, the train ticket you were given is in a language you don't understand. You should probably figure out what it says before you get to the train station after the next flight.
+#
+# Unfortunately, you can't actually **read** the words on the ticket. You can, however, read the numbers, and so you figure out **the fields these tickets must have** and **the valid ranges** for values in those fields.
+#
+# You collect the **rules for ticket fields**, the **numbers on your ticket**, and the **numbers on other nearby tickets** for the same train service (via the airport security cameras) together into a single document you can reference (your puzzle input).
+#
+# The **rules for ticket fields** specify a list of fields that exist **somewhere** on the ticket and the **valid ranges of values** for each field. For example, a rule like `class: 1-3 or 5-7` means that one of the fields in every ticket is named `class` and can be any value in the ranges `1-3` or `5-7` (inclusive, such that `3` and `5` are both valid in this field, but `4` is not).
+#
+# Each ticket is represented by a single line of comma-separated values. The values are the numbers on the ticket in the order they appear; every ticket has the same format. For example, consider this ticket:
+#
+#     .--------------------------------------------------------.
+#     | ????: 101    ?????: 102   ??????????: 103     ???: 104 |
+#     |                                                        |
+#     | ??: 301  ??: 302             ???????: 303      ??????? |
+#     | ??: 401  ??: 402           ???? ????: 403    ????????? |
+#     '--------------------------------------------------------'
+#
+# Here, `?` represents text in a language you don't understand. This ticket might be represented as `101,102,103,104,301,302,303,401,402,403`; of course, the actual train tickets you're looking at are **much** more complicated. In any case, you've extracted just the numbers in such a way that the first number is always the same specific field, the second number is always a different specific field, and so on - you just don't know what each position actually means!
+#
+# Start by determining which tickets are **completely invalid**; these are tickets that contain values which **aren't valid for any field**. Ignore **your ticket** for now.
+#
+# For example, suppose you have the following notes:
+#
+#     class: 1-3 or 5-7
+#     row: 6-11 or 33-44
+#     seat: 13-40 or 45-50
+#
+#     your ticket:
+#     7,1,14
+#
+#     nearby tickets:
+#     7,3,47
+#     40,4,50
+#     55,2,20
+#     38,6,12
+#
+# It doesn't matter which position corresponds to which field; you can identify invalid **nearby tickets** by considering only whether tickets contain **values that are not valid for any field**. In this example, the values on the first **nearby ticket** are all valid for at least one field. This is not true of the other three **nearby tickets**: the values `4`, `55`, and `12` are are not valid for any field. Adding together all of the invalid values produces your **ticket scanning error rate**: `4 + 55 + 12 = 71`.
+#
+# Consider the validity of the **nearby tickets** you scanned. **What is your ticket scanning error rate?**
+#
+# Your puzzle answer was `27911`.
+
+# %%
+input = """class: 1-3 or 5-7
+row: 6-11 or 33-44
+seat: 13-40 or 45-50
+
+your ticket:
+7,1,14
+
+nearby tickets:
+7,3,47
+40,4,50
+55,2,20
+38,6,12"""
+
+# %%
+input = open('input.16.txt', 'r').read()
+
+# %%
+import re
+
+def parse_fields(s):
+    parts = REGEX.match(s).groups()
+    assert len(parts) == 5
+    return parts[0], [[int(parts[1]), int(parts[2])], [int(parts[3]), int(parts[4])]]
+
+def is_in_fields(number):
+    for f in fields:
+        v = list(f.values())[0]
+        if v[0][0] <= number <= v[0][1]: return True
+        if v[1][0] <= number <= v[1][1]: return True
+    return False
+
+REGEX = re.compile("(.*): (.*)-(.*) or (.*)-(.*)")
+
+fields, ticket, tickets = input.split("\n\n")
+fields = fields.split("\n")
+fields = [{k: v} for f in fields for k, v in [parse_fields(f)]]
+
+ticket = [int(s) for s in ticket.split("\n")[1].split(",")]
+
+tickets = [[int(s) for s in t.split(",")] for t in tickets.split("\n")[1:]]
+tickets = np.array(tickets)
+
+not_in_fields = set([n for n in np.arange(tickets.min(), tickets.max() + 1) if not is_in_fields(n)])
+
+np.sum([n for n in tickets.flat if n in not_in_fields])
+
+# %% [markdown]
+# ### Part Two
+#
+# Now that you've identified which tickets contain invalid values, **discard those tickets entirely**. Use the remaining valid tickets to determine which field is which.
+#
+# Using the valid ranges for each field, determine what order the fields appear on the tickets. The order is consistent between all tickets: if `seat` is the third field, it is the third field on every ticket, including **your ticket**.
+#
+# For example, suppose you have the following notes:
+#
+#     class: 0-1 or 4-19
+#     row: 0-5 or 8-19
+#     seat: 0-13 or 16-19
+#
+#     your ticket:
+#     11,12,13
+#
+#     nearby tickets:
+#     3,9,18
+#     15,1,5
+#     5,14,9
+#
+# Based on the **nearby tickets** in the above example, the first position must be `row`, the second position must be `class`, and the third position must be `seat`; you can conclude that in **your ticket**, `class` is `12`, `row` is `11`, and `seat` is `13`.
+#
+# Once you work out which field is which, look for the six fields on **your ticket** that start with the word `departure`. **What do you get if you multiply those six values together?**
+#
+# Your puzzle answer was `737176602479`.
+
+# %%
+import pandas as pd
+
+def possible_fields(number):
+    possible = []
+    for f in fields:
+        k = list(f.keys())[0]
+        v = list(f.values())[0]
+        if v[0][0] <= number <= v[0][1]:
+            possible += [k]
+        if v[1][0] <= number <= v[1][1]:
+            possible += [k]
+    return possible
+
+df = pd.DataFrame(tickets)
+
+p = df.applymap(lambda t: set(possible_fields(t)))
+p = p.loc[p.applymap(lambda l: len(l)).min(axis=1).gt(0), :]
+
+cc = [set.intersection(*c) for _, c in p.iteritems()]
+
+known = [None] * len(cc)
+lcc = [len(c) for c in cc]
+while max(lcc) > 0:
+    i = lcc.index(1)
+    f = list(cc[i])[0]
+    known[i] = f
+    cc = [c - {f} for c in cc]
+    lcc = [len(c) for c in cc]
+
+np.prod([ticket[i] for i, k in enumerate(known) if k.startswith('departure')])
